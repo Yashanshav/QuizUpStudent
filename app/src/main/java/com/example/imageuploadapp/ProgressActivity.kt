c7cbd08cb16b2207c414c91fa6d77d024aec2346
Sreshtha <sreshtha.mehrotra@gmail.com>
2022-06-11 21:40:18 +0530

    fixed bug in ProgressActivity, updated adapter RV, updated model classes

--- app/src/main/java/com/example/imageuploadapp/ProgressActivity.kt
+++ app/src/main/java/com/example/imageuploadapp/ProgressActivity.kt
@@ -1,24 +1,23 @@
 package com.example.imageuploadapp
 
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.imageuploadapp.models.Result
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class ProgressActivity : AppCompatActivity() {
 
     private lateinit var adapter: ResultAdapter
     private lateinit var auth: FirebaseAuth
     private val db = FirebaseFirestore.getInstance()
+    private val listLiveData :MutableLiveData<List<Result>> = MutableLiveData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,34 +25,47 @@ class ProgressActivity : AppCompatActivity() {
 
         Log.d("PROGRESS ACTIVITY", "PROGRESS ACTIVITY STARTED")
         auth = FirebaseAuth.getInstance()
-
         setUpRecyclerView()
+
+        listLiveData.observe(this) {
+            adapter.list = it
+            adapter.notifyDataSetChanged()
+        }
 
     }
 
+
+    override fun onResume() {
+        super.onResume()
+        setUpRecyclerView()
+        getDataFromFireStore()
+    }
+
     private fun setUpRecyclerView() {
+        adapter = ResultAdapter()
+        val recyclerViewr = findViewById<RecyclerView>(R.id.recyclerViewR)
+        recyclerViewr.adapter = adapter
+        recyclerViewr.layoutManager =
+            LinearLayoutManagerWrapper(this@ProgressActivity, LinearLayoutManager.VERTICAL, false)
+    }
 
-
-        val resCollection = db.collection("results")
-        val query = resCollection.orderBy("givenBy")
-        var recyclerOptions: FirestoreRecyclerOptions<Result>
-
-        GlobalScope.launch(Dispatchers.IO) {
-            recyclerOptions = FirestoreRecyclerOptions.Builder<Result>().setQuery(query, Result::class.java).build()
-            adapter = ResultAdapter(recyclerOptions)
-
-            val recyclerViewr = findViewById<RecyclerView>(R.id.recyclerViewR)
-
-            recyclerViewr.adapter = adapter
-            recyclerViewr.layoutManager =
-                LinearLayoutManagerWrapper(this@ProgressActivity, LinearLayoutManager.VERTICAL, false)
+    private fun getDataFromFireStore(){
+        db.collection("results").get().addOnCompleteListener {
+            if(it.isSuccessful){
+                Log.d("MAIN__","success")
+                val result = mutableListOf<Result>()
+                it.result.documents.forEach{
+                    Log.d("MAIN__",it.data.toString())
+                    val doc = it.toObject(Result::class.java)
+                    if (doc != null) {
+                        result.add(doc)
+                    }
+                }
+                listLiveData.value = result
+            }
+            else{
+                Log.d("MAIN__","success")
+            }
         }
-
-
-//        Log.d("not null", recyclerOptions.toString())
-
-        // Now the recycler options are pass in adapter to display in firestore recycler view
-
-
     }
 }
